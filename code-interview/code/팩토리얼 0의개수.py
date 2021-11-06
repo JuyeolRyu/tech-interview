6a13e74d56d7958634ea1ae138ac88ab08f6c54d
JuyeolRyu <yujuyeol00@naver.com>
2021-11-06 19:10:52 +0900

    Update 팩토리얼 0의개수.py

--- code-interview/code/팩토리얼 0의개수.py
+++ code-interview/code/팩토리얼 0의개수.py
@@ -19,7 +19,7 @@ for c in list(reversed(str(num))):
     break
 print(ans)
 
-#sol2
+#sol2 최대공약수
 ans = 0
 i = 1
 while 5**i <= n:
@@ -32,3 +32,23 @@ for num in range(1,n+1):
       break
 
 print(ans)
+
+#sol3 최대공약수,dp
+def solution(n):
+    ans=0
+    dp=[-1]*(n+1)
+    def find(num):
+        if dp[num]!=-1:
+            return dp[num]
+        
+        if num%5==0:
+            dp[num]=dp[num//5]+1
+        else:
+            dp[num]=0
+        return dp[num]
+    for i in range(1,n+1):
+        find(i)
+    
+    ans=sum(dp[1:])
+    print(ans)
+    return ans
